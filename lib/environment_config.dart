d92c58d4fbbd49456a215a7d2de6caac8fa534cb
Nursat <158581107+Nur1sat@users.noreply.github.com>
2025-02-28 11:40:32 +0500

    Update environment_config.dart
    deleting api_key

--- lib/environment_config.dart
+++ lib/environment_config.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class EnvironmentConfig {
-  final movieApiKey = '40fc774d8db82a35dbde9967daeaf2c6';
+  final movieApiKey = 'api_key';
 }
 
 final environmentConfigProvider = Provider<EnvironmentConfig>((ref) {
